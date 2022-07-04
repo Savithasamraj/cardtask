40ab098b4f923f6f983ab51ac27858dbdfda4231
Savithasamraj <ssavitha1905@gmail.com>
2022-07-04 12:09:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Card from "./Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  let arr = [
+    {
+      first: "FREE",
+      second: "$50/month",
+      third: "single User",
+      features:[{
+          title: "5GB Storage",
+          able:true
+      },
+    {
+        title: " Unlimited Public Projects" ,
+        able:true
+    },
+    {
+        title:  "Dedicated Phone Support",
+        
+        able:true
+    },
+    {
+        title: "Dedicated Phone Support" ,
+        able:true
+    },
+    {
+        title:"Free Subdomain" ,
+        able:false
+    },{
+        title:" Monthly Status Reports" ,
+        able:false
+    }]
+},
+     
+    {
+      first: "PLUS",
+
+      second: "$9/month",
+      third: "5 User",
+      features:[{
+        title: "5GB Storage",
+        able:true
+    },
+  {
+      title: " Unlimited Public Projects" ,
+      able:true
+  },
+  {
+      title:  "Dedicated Phone Support",
+      able:true
+  },
+  {
+      title: "Dedicated Phone Support" ,
+      able:false
+  },
+  {
+      title:"Free Subdomain" ,
+      able:false
+  },{
+      title:" Monthly Status Reports" ,
+      able:false
+  }]
+    },
+    {
+      first: "Pro",
+
+      second: "$49/month",
+      third: "unlimited User",
+      features:[{
+        title: "5GB Storage",
+        able:true
+    },
+  {
+      title: " Unlimited Public Projects" ,
+      able:true
+  },
+  {
+      title:  "Dedicated Phone Support",
+      able:true
+  },
+  {
+      title: "Dedicated Phone Support" ,
+      able:true
+  },
+  {
+      title:"Free Subdomain" ,
+      able:true
+  },{
+      title:" Monthly Status Reports" ,
+      able:false
+  }]
+    },
+  ];
+  return (
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {arr.map((pro) => {
+            return (
+              <Card data={pro}>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,86 @@
+function Card({data}) {
+  return (
+    <div class="col-lg-4">
+      <div class="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">
+            {data.first}
+          </h5>
+          <h6 className="card-price text-center">
+            {" "}
+            {data.second}
+            <span className="period">{data.third}</span>
+          </h6>
+          <hr></hr>
+         <ul className="fa-ul">{
+           data.features.map((list)=>{
+
+            return <li className={list.able?"":"text-muted"}>
+              {
+                // list.able?<FontAwesomeIcon icon={["fa-ul-check"]} />:<FontAwesomeIcon icon={["fa-ul-times"]} />
+              }
+            {/* <span className="fa-li">
+              <i className="fas fa-check"></i>
+             
+            </span> */}
+         {list.title}
+          
+          </li>  
+           })
+          
+
+         }
+            {/* <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.five}
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.six}
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.seven}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.eight}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.nine}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.nine}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.ten}
+            </li> */}
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">
+              Button
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
