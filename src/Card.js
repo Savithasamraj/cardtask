0cde037fdf69fae0e51e8aaf8b13c4ce915c96b4
Savithasamraj <ssavitha1905@gmail.com>
2022-07-04 12:23:13 +0530

    first commit

--- src/Card.js
+++ src/Card.js
@@ -74,7 +74,7 @@ function Card({data}) {
             </li> */}
           </ul>
           <div className="d-grid">
-            <a href="#" className="btn btn-primary text-uppercase">
+            <a  className="btn btn-primary text-uppercase">
               Button
             </a>
           </div>
